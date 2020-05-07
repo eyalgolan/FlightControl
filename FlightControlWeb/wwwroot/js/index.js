7f68d6f0595b2dbda59fcd3539374da5ac284f2d
Sapir Deutscher <sapiroosh.d@gmail.com>
2020-05-07 21:28:04 +0300

    added frontend files 7.5

---
+++ FlightControlWeb/wwwroot/js/index.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    initMap();
+})
+function initMap() {
+    var map = L.map('map', {
+        center: [20.0, 5.0],
+        minZoom: 2,
+        zoom: 2
+    });
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains: ['a', 'b', 'c']
+    }).addTo(map);
+
+    $.getJSON('json/markers.json', function (markers) {
+        debugger;
+        for (var i = 0; i < markers.length; ++i) {
+            L.marker([markers[i].lat, markers[i].lng]).
+                bindPopup('<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>').
+                addTo(map);
+        }
+    });
+}
