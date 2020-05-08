d9984bf56f629a901bfa039b1bdee3858c001cfb
Eyal Golan <eyalgolan96@gmail.com>
2020-05-08 15:51:20 +0300

    start merging both projects

--- FlightControlWeb/Models/Flight.cs
+++ FlightControlWeb/Models/Flight.cs
@@ -1,39 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FlightControlWeb.Models
 {
     public class Flight
     {
-        private FlightPlan fp;
-        private int id;
-        private static int idCouner = 0;
-        public int FlightId
+        private FlightPlan flightPlan;
+        private bool isExternal;
+
+        public Flight(FlightPlan fpInput, bool isExternalInput)
         {
-            get
-            {
-                return this.id;
-            }
-            set
-            {
-                this.id = value;
-            }
+            flightPlan = fpInput;
+            isExternal = isExternalInput;
+            FlightId = CreateID();
         }
+
+        public string FlightId { get; set; }
+
         public double Longtitude { get; set; }
         public double Latitude { get; set; }
         public int Passengers { get; set; }
-        public String CompanyName { get; set; }
-        public String DateTime { get; set; }
-        public Boolean IsExternal { get; set; }
+        public string CompanyName { get; set; }
+        public string DateTime { get; set; }
+        public bool IsExternal { get; set; }
         public FlightPlan Fp { get; set; }
 
-        public Flight (FlightPlan fpInput)
+        private string CreateID()
         {
-            this.fp = fpInput;
-            this.id = idCouner;
-            idCouner++;
+            var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var rand = new Random();
+
+            var id = "";
+            for (var i = 0; i < 2; i++)
+            {
+                var j = rand.Next(0, letters.Length - 1);
+                id += letters[j];
+            }
+
+            id += rand.Next(1000, 99999999).ToString();
+            return id;
         }
     }
 }
