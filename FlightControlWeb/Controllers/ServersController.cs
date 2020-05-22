821a12d061285c576285d0f6a6fe2f6e9dd7eaef
Ofir Netzer <ofir.netz@gmail.com>
2020-05-22 13:40:21 +0300

    ServerController with documentation

--- FlightControlWeb/Controllers/ServersController.cs
+++ FlightControlWeb/Controllers/ServersController.cs
@@ -19,6 +19,11 @@ namespace FlightControlWeb.Controllers
         }
 
         // GET: api/Servers
+        /* 
+        * This method is our implementation of HTTP GET command.
+        * once the user asks for the entire servers we return a list of all the servers
+        * that we have active flights from in our DBs.
+        */
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Server>>> GetAllServers()
         {
@@ -28,6 +33,12 @@ namespace FlightControlWeb.Controllers
         // POST: api/Servers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+
+        /* 
+        * This method is our implementation of HTTP POST command.
+        * when we want to add a server to our program we use it.
+        * if the server is already exist in our DB we return conflict
+        */
         [HttpPost]
         public async Task<ActionResult<Server>> PostServer(Server server)
         {
@@ -52,6 +63,13 @@ namespace FlightControlWeb.Controllers
         }
 
         // DELETE: api/Servers/5
+
+        /* 
+        * This method is our implementation of HTTP DELETE command.
+        * when we want to delete a server from our DB we check if it exists.
+        * if it doesn't we return not found. otherwise it removes the server
+        * from our DB
+        */
         [HttpDelete("{id}")]
         public async Task<ActionResult<Server>> DeleteServer(string serverId)
         {
@@ -67,6 +85,9 @@ namespace FlightControlWeb.Controllers
             return server;
         }
 
+        /* 
+        * This method returns true if the given server exist in our DB, false otherwise.
+        */
         private bool ServerExists(string id)
         {
             return _context.serverItems.Any(e => e.ServerID == id);
