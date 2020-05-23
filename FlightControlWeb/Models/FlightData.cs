be4c452a2a0e7c28d794a557eec35846f4262c15
Ofir Netzer <ofir.netz@gmail.com>
2020-05-23 16:08:44 +0300

    JSON names are according to pavel's demand (and obeys the coding style)

--- FlightControlWeb/Models/FlightData.cs
+++ FlightControlWeb/Models/FlightData.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace FlightControlWeb.Models
 {
     public class FlightData
-    { 
+    {
+        [JsonPropertyName("flight_id")]
         public string flight_id { get; set; }
+        [JsonPropertyName("is_external")]
         public bool IsExternal { get; set; }
+        [JsonPropertyName("longitude")]
         public double longitude { get; set; }
+        [JsonPropertyName("latitude")]
         public double latitude { get; set; }
+        [JsonPropertyName("passengers")]
         public int passengers { get; set; }
+        [JsonPropertyName("company_name")]
         public string company_name { get; set; }
+        [JsonPropertyName("date_time")]
         public DateTime date_time { get; set; }
     }
 }
